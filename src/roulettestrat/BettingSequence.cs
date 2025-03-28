a3f826404b2a748b004dee91d85f878a01e7c922
Cedric Davies <cedricda@ntdev.microsoft.com>
2025-03-28 11:28:04 -0700

    DoubleDozenBettingSequence

---
+++ src/roulettestrat/BettingSequence.cs
@@ -0,0 +1,76 @@
+﻿namespace roulettestrat;
+
+public interface IBettingSequence
+{
+    IEnumerable<IBet> Bets { get; }
+
+    bool IsComplete { get; }
+
+    int SequenceTotalAmount { get; } // Negative if lost money
+
+    void SetSpinResult(SpinResult spin);
+
+    void CreateNextBet(Func<int, IEnumerable<SpinResult>> getSpinHistoryFunc);
+}
+
+public class DoubleDozenBettingSequence(BankRoll bankRoll) : IBettingSequence
+{
+    private readonly int[] betProgression = [4, 15, 50, 150, 300];
+    private readonly List<DoubleDozenBet> bets = [];
+
+    private int currentBetIndex = -1;
+    private bool currentBetHasSpinResult;
+    private int losingDozen = -1;
+
+    public IEnumerable<IBet> Bets => bets.AsReadOnly();
+    public bool IsComplete { get; protected set; }
+    public int SequenceTotalAmount { get; protected set; }
+
+    public void SetSpinResult(SpinResult spin)
+    {
+        if (this.currentBetHasSpinResult) throw new InvalidOperationException("current bet already has a spin result");
+
+        var currentBet = this.bets[this.currentBetIndex];
+        currentBet.SetSpinResult(spin);
+        this.currentBetHasSpinResult = true;
+
+        if (currentBet.Win)
+        {
+            this.IsComplete = true;
+            this.SequenceTotalAmount += currentBet.WinAmount;
+            bankRoll.AddBetWin(currentBet.BetAmount + currentBet.WinAmount);
+        }
+        else
+        {
+            this.SequenceTotalAmount -= currentBet.BetAmount;
+
+            // Check if we have reached the end of our sequence. This is a total loss scenario.
+            if (this.currentBetIndex == this.betProgression.Length - 1)
+            {
+                this.IsComplete = true;
+            }
+        }
+    }
+
+    public void CreateNextBet(Func<int, IEnumerable<SpinResult>> getSpinHistoryFunc)
+    {
+        if (this.IsComplete) throw new InvalidOperationException("Betting sequence is complete");
+
+        if (this.currentBetIndex > -1 && !this.currentBetHasSpinResult) throw new InvalidOperationException("We have a current bet that has not received a spin result");
+
+        // If this is the first bet - we decide which 2 dozens to bet on and stick with it for the rest of the sequence.
+        if (this.losingDozen == -1)
+        {
+            int spinNumber = getSpinHistoryFunc(1).FirstOrDefault().Number;
+            if (spinNumber == 0) spinNumber = 1; // If number is 0, then choose the first dozen
+
+            this.losingDozen = ((spinNumber - 1) / 12) + 1; // We bet against the last dozen
+        }
+
+        int betAmount = this.betProgression[++this.currentBetIndex];
+        bankRoll.TakeCashForBet(betAmount * 2); // bet this amount on each of our two dozens
+        var bet = new DoubleDozenBet(betAmount, this.losingDozen);
+        this.bets.Add(bet);
+        this.currentBetHasSpinResult = false;
+    }
+}
---
+++ src/roulettestratTest/DoubleDozenBettingSequenceTest.cs
@@ -0,0 +1,143 @@
+﻿namespace roulettestratTest;
+
+using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+using roulettestrat;
+
+public class DoubleDozenBettingSequenceTest
+{
+    [Fact]
+    public void WinFirstSpin()
+    {
+        var lastSpin = new SpinResult(30);
+        var bank = new BankRoll(2000);
+
+        var sequence = new DoubleDozenBettingSequence(bank);
+        sequence.IsComplete.Should().BeFalse();
+        sequence.CreateNextBet(_ => [lastSpin]);
+        sequence.SetSpinResult(new SpinResult(20));
+
+        sequence.IsComplete.Should().BeTrue();
+        sequence.SequenceTotalAmount.Should().Be(4);
+        sequence.Bets.Count().Should().Be(1);
+    }
+
+    [Fact]
+    [SuppressMessage("ReSharper", "AccessToModifiedClosure")]
+    public void TotalLossSequence()
+    {
+        // Total loss is losing 5 times. So five times in the losing dozen or on 0.
+
+        var lastSpinResult = new SpinResult(5);
+        var bank = new BankRoll(2000);
+
+        var sequence = new DoubleDozenBettingSequence(bank);
+        
+        // loss 1
+        sequence.CreateNextBet(_ => [lastSpinResult]);
+        sequence.SetSpinResult(lastSpinResult = new SpinResult(0, SpinZero.Double));
+
+        sequence.IsComplete.Should().BeFalse();
+
+        // loss 2
+        sequence.CreateNextBet(_ => [lastSpinResult]);
+        sequence.SetSpinResult(lastSpinResult = new SpinResult(10));
+
+        sequence.IsComplete.Should().BeFalse();
+        sequence.Bets.Last().Win.Should().BeFalse();
+        sequence.Bets.Last().BetAmount.Should().Be(30);
+
+        // loss 3
+        sequence.CreateNextBet(_ => [lastSpinResult]);
+        sequence.SetSpinResult(lastSpinResult = new SpinResult(8));
+
+        sequence.IsComplete.Should().BeFalse();
+        sequence.SequenceTotalAmount.Should().Be(-69 * 2);
+
+        // loss 4
+        sequence.CreateNextBet(_ => [lastSpinResult]);
+        sequence.SetSpinResult(lastSpinResult = new SpinResult(0, SpinZero.Single));
+
+        sequence.IsComplete.Should().BeFalse();
+
+        // loss 5
+        sequence.CreateNextBet(_ => [lastSpinResult]);
+        sequence.SetSpinResult(lastSpinResult = new SpinResult(4));
+
+        sequence.IsComplete.Should().BeTrue();
+        sequence.SequenceTotalAmount.Should().Be((4 + 15 + 50 + 150 + 300) * -2);
+    }
+
+    [Fact]
+    public void WinOnLastProgressionBet()
+    {
+        var lastSpinResult = new SpinResult(5);
+        var bank = new BankRoll(2000);
+        var sequence = new DoubleDozenBettingSequence(bank);
+
+        for (int i = 0; i < 4; i++)
+        {
+            sequence.CreateNextBet(_ => [lastSpinResult]);
+            sequence.SetSpinResult(lastSpinResult);
+            sequence.IsComplete.Should().BeFalse();
+        }
+
+        // Now win on the last bet
+        sequence.CreateNextBet(_ => [lastSpinResult]);
+        sequence.SetSpinResult(new SpinResult(27));
+
+        sequence.IsComplete.Should().BeTrue();
+        sequence.SequenceTotalAmount.Should().Be((4 + 15 + 50 + 150) * -2 + 300); // -138
+    }
+
+    [Fact]
+    public void NoBetAfterComplete()
+    {
+        var lastSpinResult = new SpinResult(5);
+        var bank = new BankRoll(2000);
+        var sequence = new DoubleDozenBettingSequence(bank);
+        
+        sequence.CreateNextBet(_ => [lastSpinResult]);
+        sequence.SetSpinResult(new SpinResult(31));
+        sequence.IsComplete.Should().BeTrue();
+        Action act = () => sequence.CreateNextBet(_ => [lastSpinResult]);
+        act.Should().Throw<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void NoBetIfLastBetNoSpin()
+    {
+        var lastSpinResult = new SpinResult(5);
+        var bank = new BankRoll(2000);
+        var sequence = new DoubleDozenBettingSequence(bank);
+
+        sequence.CreateNextBet(_ => [lastSpinResult]);
+        Action act = () => sequence.CreateNextBet(_ => [lastSpinResult]);
+        act.Should().Throw<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void NoDoubleSpin()
+    {
+        var lastSpinResult = new SpinResult(5);
+        var bank = new BankRoll(2000);
+        var sequence = new DoubleDozenBettingSequence(bank);
+
+        sequence.CreateNextBet(_ => [lastSpinResult]);
+        sequence.SetSpinResult(new SpinResult(30));
+        Action act = () => sequence.SetSpinResult(new SpinResult(22));
+        act.Should().Throw<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void NoSpinHistory()
+    {
+        var bank = new BankRoll(2000);
+        var sequence = new DoubleDozenBettingSequence(bank);
+
+        sequence.CreateNextBet(_ => Enumerable.Empty<SpinResult>()); // If no history we choose first dozen to bet against.
+        sequence.SetSpinResult(new SpinResult(30));
+        sequence.IsComplete.Should().BeTrue();
+        sequence.SequenceTotalAmount.Should().Be(4);
+    }
+}
